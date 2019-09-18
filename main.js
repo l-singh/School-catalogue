f728e6f78eb61d74c80e2a0c4f904461d2030626
lovely singh <17668123+l-singh@users.noreply.github.com>
2019-09-18 10:42:36 -0700

    Update main.js

--- main.js
+++ main.js
@@ -17,23 +17,23 @@ class School {
     return this._numberOfStudents;
   }
   
+  set numberOfStudents(value) {
+    if(value.isNaN){
+      console.log('Invalid input: numberOfStudents must be set to a Number.');
+    }
+    else {
+      this.numberOfStudents = value;
+    }
+}
+  
   quickFacts() {
-    console.log(`${_name} educates ${_numberOfStudents} students at the ${_level} school level.`);
+    console.log(`${this.name} educates ${this.numberOfStudents} students at the ${this.level} school level.`);
   }
   
   static pickSubstituteTeacher(substituteTeachers) {
    let index = Math.floor(substituteTeachers.length * Math.random());
     return substituteTeachers[index];
   }
-  
-  set numberOfStudents(value) {
-    if(value === number){
-      return this.value = numberOfStudents;
-    }
-    else {
-      console.log('Invalid input: numberOfStudents must be set to a Number.');
-    }
-}
 }
 
 class PrimarySchool extends School{
@@ -60,3 +60,6 @@ class HighSchool extends School {
 
 const lorraineHansbury = new PrimarySchool('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
 lorraineHansbury.quickFacts();
+School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']);
+const alSmith = new HighSchool('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field']);
+alSmith.sportsTeams;
